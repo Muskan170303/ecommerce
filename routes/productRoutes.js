b6f65918e92149f7c28994e711f668de7b7d958b
Muskan Goyal <muskangoyal170303@gmail.com>
2024-01-27 09:57:48 +0530

    commit 6: error page and validations

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -2,52 +2,92 @@ const express=require('express')
 const Product=require('../models/Product')
 const Review=require('../models/Review')
 const router=express.Router();  //mini instance
+const {validateProduct}=require('../middleware')
+
+router.get('/error',(req,res)=>{
+    res.render('error')
+})
 
 router.get('/products', async (req,res)=>{
-    let products = await Product.find({});
-    res.render('products/index',{products});
+    try{
+        let products = await Product.find({});
+        res.render('products/index',{products});
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
 router.get('/product/new', (req,res)=>{
-    res.render('products/new')
+    try{
+        res.render('products/new')
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
-router.post('/products', async (req,res)=>{
-    let {name, img, price, desc}=req.body;
-    await Product.create({name, img, price, desc});
-    res.redirect('/products')
+router.post('/products',validateProduct, async (req,res)=>{
+    try{
+        let {name, img, price, desc}=req.body;
+        await Product.create({name, img, price, desc});
+        res.redirect('/products')
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
 router.get('/products/:id', async (req,res)=>{
-    let {id}=req.params;
-    let foundPro= await Product.findById(id).populate('reviews');
-    res.render('products/show',{foundPro});
+    try{
+        let {id}=req.params;
+        let foundPro= await Product.findById(id).populate('reviews');
+        res.render('products/show',{foundPro});
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
 router.get('/products/:id/edit' , async (req,res)=>{
-    let {id}=req.params; 
-    let foundPro= await Product.findById(id);
-    res.render('products/edit',{foundPro});
+    try{ 
+        let {id}=req.params; 
+        let foundPro= await Product.findById(id);
+        res.render('products/edit',{foundPro});
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
-router.patch('/products/:id' , async(req,res)=>{
-    let {id}=req.params; 
-    let {name, img, price, desc}=req.body;
-    await Product.findByIdAndUpdate(id,{name, img, price, desc});
-    res.redirect(`/products/${id}`)
+router.patch('/products/:id' , validateProduct, async(req,res)=>{
+    try{
+        let {id}=req.params; 
+        let {name, img, price, desc}=req.body;
+        await Product.findByIdAndUpdate(id,{name, img, price, desc});
+        res.redirect(`/products/${id}`)
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
 router.delete('/products/:id', async(req,res)=>{
-    let {id}=req.params;
+    try{
+        let {id}=req.params;
 
-    // for removing reviews before product
-    // const product=await Product.findById(id);
-    // for(let id of product.reviews){
-    //     await Review.findByIdAndDelete(id)
-    // }
+        // for removing reviews before product
+        // const product=await Product.findById(id);
+        // for(let id of product.reviews){
+        //     await Review.findByIdAndDelete(id)
+        // }
 
-    await Product.findByIdAndDelete(id);
-    res.redirect('/products')
+        await Product.findByIdAndDelete(id);
+        res.redirect('/products')
+    }
+    catch(e){
+        res.status(500).render('error',{err:e.message})
+    }
 })
 
 module.exports=router;
---
+++ schema.js
@@ -0,0 +1,16 @@
+// schema for server side validation
+const Joi = require('joi');
+
+const productSchema = Joi.object({
+    name:Joi.string() .required(),
+    img:Joi.string().required(),
+    price:Joi.number().min(0).required(),
+    desc:Joi.string()
+})
+
+const reviewSchema = Joi.object({
+    rating:Joi.string().required().min(0).max(5),
+    comment:Joi.string(),
+})
+
+module.exports ={productSchema,reviewSchema}
